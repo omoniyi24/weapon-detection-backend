e29c154810271b230cf20412d00e045994dde0a9
Omoniyi Ilesanmi <omoniyi.ilesanmi@prunny.com>
2024-09-12 20:06:01 +0100

    implement trained yolov8

---
+++ YOLO_Video.py
@@ -0,0 +1,40 @@
+from ultralytics import YOLO
+import cv2
+import math
+
+def video_detection(path_x):
+    video_capture = path_x
+    cap=cv2.VideoCapture(video_capture)
+
+    model=YOLO("yolov8.pt")
+    classNames = ["gun", "knife"]
+    while True:
+        success, img = cap.read()
+        results=model(img,stream=True)
+        for r in results:
+            boxes=r.boxes
+            for box in boxes:
+                x1,y1,x2,y2=box.xyxy[0]
+                x1,y1,x2,y2=int(x1), int(y1), int(x2), int(y2)
+                print(x1,y1,x2,y2)
+                cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255),3)
+                conf=math.ceil((box.conf[0]*100))/100
+                cls=int(box.cls[0])
+                class_name=classNames[cls]
+                label=f'{class_name}{conf}'
+                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                print(t_size)
+                c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                if class_name == 'gun':
+                    color=(0, 204, 255)
+                elif class_name == "knife":
+                    color = (222, 82, 175)
+                else:
+                    color = (85,45,255)
+                if conf>0.5:
+                    cv2.rectangle(img, (x1,y1), (x2,y2), color,3)
+                    cv2.rectangle(img, (x1,y1), c2, color, -1, cv2.LINE_AA)
+                    cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
+        yield img
+
+cv2.destroyAllWindows()
